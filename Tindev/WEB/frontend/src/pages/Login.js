ebb1744f8a59585fa9a4e0e2342351ed3315286a
Matheusmlx <matheusluiz0303@outlook.com>
2019-08-11 18:15:58 -0400

    Puxando Users da API

--- Tindev/WEB/frontend/src/pages/Login.js
+++ Tindev/WEB/frontend/src/pages/Login.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import api from '../services/api'
 import logo from "../assets/logo.svg";
 
 import "./Login.css";
 
-export default function Login() {
+export default function Login({ history }) {
+  const [username, setUsername] = useState('');
+
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post('/devs', {
+      username,
+    })
+
+
+
+    const { _id } = response.data
+    console.log(_id)
+    //redirecionar o usuario para a rota 
+    history.push(`/dev/${_id}`)
+
+  }
+
   return (
     <div className="login-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <img src={logo} alt="Tindev" />
         <input
           placeholder="Digite o username do seu git"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
         />
         <button type="submit">Enviar</button>
-
       </form>
 
     </div>
@@ -21,3 +42,5 @@ export default function Login() {
   );
 
 }
+
+//Componente , Estado e Propriedades
--- Tindev/WEB/frontend/src/pages/Main.js
+++ Tindev/WEB/frontend/src/pages/Main.js
@@ -1,5 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import logo from "../assets/logo.svg";
+import like from "../assets/like.svg";
+import api from '../services/api'
+import dislike from "../assets/dislike.svg";
+import './Main.css';
+//com esse match você tem acesso a todos os parametros passsados para esssa rota
+export default function Main({ match }) {
+    const [users, setUsers] = useState([])
 
-export default function Main() {
-    return <h1>Hello World</h1>
+    useEffect(() => {
+        async function loadUsers() {
+            const response = await api.get('/devs', {
+                headers: {
+                    user: match.params.id
+                }
+            })
+            setUsers(response.data)
+        }
+        loadUsers();
+    }, [match.params.id])
+    return (
+        <div className="main-container">
+            <img src={logo} alt="Tindev" />
+            <ul>
+                {users.map(user => (
+                    <li key={user._id}>
+                        <img src={user.avatar} alt={user.name} />
+                        <footer>
+                            <strong>{user.name}</strong>
+                            <p>{user.bio}</p>
+                        </footer>
+                        <div className="buttons">
+                            <button type="button">
+                                <img src={dislike} alt="dislike" />
+                            </button>
+                            <button type="button">
+                                <img src={like} alt="like" />
+                            </button>
+                        </div>
+
+                    </li>
+                ))}
+
+
+            </ul>
+
+
+        </div>
+
+    );
 }
